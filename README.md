fc9a5f8b34ef57ede915daf7b3f913db6cfd148c
namuyan <thhjuu@yahoo.co.jp>
2017-11-13 22:01:00 +0900

    ver 2.0

--- README.md
+++ README.md
@@ -19,13 +19,55 @@ python3 tipnem.py
 ***
 
 ## ソースに組み込む。
+* `request` の送り方
 ```python
 from tipnem import WebSocketClient
 ws = WebSocketClient("ws://153.122.86.46:8088")
+ws.start()
  
 # コマンドをリクエスト(okがTrueでなければresultにエラー文)
 ok, result = ws.request("bot/info")
+ 
+# リクエストをBlockingせずに使用(意味あるｺﾚ)
+uuid = ws.request("bot/info", blocking=False)
+ok, result = ws.blocking(uuid)
 ```
+
+Streamingを取得するには
+```python
+from tipnem import WebSocketClient
+ws = WebSocketClient("ws://153.122.86.46:8088")
+ws.start()
+ 
+while True:
+    cmd, data, time_sec = ws.streaming_que.get()
+```
+
+ログインするには
+```python
+from tipnem import WebSocketClient
+ws = WebSocketClient("ws://153.122.86.46:8088")
+ws.start()
+ 
+# PINコードでログイン
+ws.login_by_pin_guest(screen="example_name") # level1
+ws.login_by_pin_user() # level2
+ 
+# 公開鍵でログイン
+seckey = "2fa58d280902095a28601840ff392381d5313716ef0dbf06beb202bc2a880f9b"
+pubkey = "06cdabf0fd421d88983f7beb849cd804a4876276728d44473d62b47f4146c3ac"
+if ws.login_by_key(seckey=seckey, pubkey=pubkey, screen="example_name"):
+    print("login成功 level:%d" % ws.level)
+else:
+    print("login失敗!")
+```
+## 履歴
+* ver 2.0  
+    streamingの取得方法を変更、queueによりSocketのブロッキングを防ぐ。  
+    PIN認証、や公開鍵認証でログインできるように。
+    
+* ver 1.0  
+    公開開始
 
 ## Licence
 MIT
--- tipnem.py
+++ tipnem.py
@@ -1,8 +1,10 @@
 #!/user/env python3
 # -*- coding: utf-8 -*-
 
+from ed25519 import Ed25519
 import websocket
 import threading
+import queue
 import logging
 import time
 import json
@@ -13,7 +15,7 @@ Tipnemを扱うライブラリ
 2017/11/07
 """
 
-version = "1.0"
+version = "2.0"
 author = "namuyan"
 
 
@@ -21,7 +23,9 @@ class WebSocketClient:
     timeout = 60
     result = dict()
     result_lock = threading.Lock()
+    streaming_que = queue.LifoQueue(maxsize=1000)
     user_code = None
+    level = 0
     height = 0
 
     def __init__(self, url, test=False):
@@ -29,7 +33,7 @@ class WebSocketClient:
         self.test = test
         self.ws = None
 
-    def create_connect(self):
+    def _create_connect(self):
         retry = 0
         while True:
             try:
@@ -46,13 +50,78 @@ class WebSocketClient:
                 logging.error("# retry connect to tipnem after 180s, %s" % retry)
                 time.sleep(180)
 
+    def login_by_key(self, seckey, pubkey, screen):
+        sign = Ed25519.sign(message=self.user_code, secret_key=seckey, public_key=pubkey)
+        data = {
+            "screen_name": "@" + screen,
+            "sign": sign.decode()
+        }
+        ok, result = self.request(command="user/pubkey/login", data=data)
+        if not ok:
+            logging.error("pubkey login %s" % result)
+            return False
+        if result['level'] > 0:
+            logging.info("# auto login! level %d" % result['level'])
+            self.level = result['level']
+            return True
+        return False
+
+    def login_by_pin_user(self, level=2):
+        while True:
+            print()
+            ok, result = self.request(command="user/upgrade", data={"require_level": level})
+            if not ok:
+                print("# failed upgrade request: ", result)
+                continue
+
+            while True:
+                pin_code = input("pincode >> ")
+                if len(pin_code) != 4:
+                    print("# need 4 letters")
+                    continue
+                break
+            data = {"pincode": int(pin_code)}
+            ok, result = self.request(command="user/check", data=data)
+            if not ok:
+                print("# failed upgrade: ", result)
+                continue
+
+            print("# upgrade OK!")
+            self.level = level
+            return
+
+    def login_by_pin_guest(self, screen):
+        while True:
+            print()
+            data = {"screen_name": "@" + screen}
+            ok, result = self.request(command="user/offer", data=data)
+            if not ok and result != "pincode has been already created.":
+                print("# failed login request: ", result)
+                continue
+
+            while True:
+                pin_code = input("pincode >> ")
+                if len(pin_code) != 4:
+                    print("# need 4 letters")
+                    continue
+                break
+            data = {"screen_name": "@" + screen, "pincode": int(pin_code)}
+            ok, result = self.request(command="user/check", data=data)
+            if not ok:
+                print("# failed login: ", result)
+                continue
+
+            print("# login OK!")
+            self.level = 1
+            return
+
     def start(self, name="tipnem"):
         threading.Thread(
-            target=self.create_connect, name=name, daemon=True
+            target=self._create_connect, name=name, daemon=True
         ).start()
         while self.ws is None:
             time.sleep(0.2)
-        logging.debug("# create ws connection")
+        logging.info("# create ws connection")
 
     def blocking(self, uuid):
         time_span = 0.005  # 1mS
@@ -75,6 +144,7 @@ class WebSocketClient:
            "data": data,
            "uuid": uuid
         }
+        logging.debug("id:%d,msg:%s" % (uuid, message))
         self.ws.send(json.dumps(message))
         if blocking is False:
             return uuid
@@ -82,15 +152,16 @@ class WebSocketClient:
         time_span = 0.005  # 1mS
         for c in range(int(self.timeout / time_span)):
             time.sleep(time_span)
-            with self.result_lock:
-                if uuid not in self.result:
-                    continue
-                else:
+            if uuid not in self.result:
+                continue
+            else:
+                with self.result_lock:
                     r = self.result[uuid]
                     del(self.result[uuid])
-                return r['result'], r['data']
-        else:
-            raise TimeoutError("[%s] [%s]" % (command, data))
+                    return r['result'], r['data']
+
+        logging.error("result: %s" % self.result)
+        raise TimeoutError("timeout [%s] [%s] [%s]" % (uuid, command, data))
 
     """ request """
     @staticmethod
@@ -114,14 +185,15 @@ class WebSocketClient:
 
         elif self.check_key(('type', 'command'), data):
             if data['type'] == 'streaming':
-                if data['command'] == 'nis/block':
-                    self.on_nis_block(ws, data['data'])
-                elif data['command'] == 'nis/incoming':
-                    self.on_nis_incoming(ws, data['data'])
-                elif data['command'] == 'tip/receive':
-                    self.on_tip_receive(ws, data['data'])
-                else:
-                    raise Exception("unknown command '%s'" % data['command'])
+                try:
+                    item = (data['command'], data['data'], data['time'])
+                    self.streaming_que.put_nowait(item=item)
+                except queue.Full:
+                    pass
+                except Exception as e:
+                    logging.error(e)
+                    import traceback
+                    traceback.print_exc()
 
             elif data['type'] == 'request':
                 with self.result_lock:
@@ -136,69 +208,55 @@ class WebSocketClient:
 
     def on_error(self, ws, error):
         logging.error("error: %s" % error)
-        # self.ws.close()
+        self.ws.close()
 
     def on_close(self, ws):
-        logging.debug("close: %s" % ws)
+        logging.info("close: %s" % ws)
         # self.ws.close()
         # time.sleep(5)
-        # logging.debug("try reconnect")
+        # logging.info("try reconnect")
         # self.create_connect()
 
     def on_open(self, ws):
         self.ws = ws
-        logging.debug("open: %s" % ws)
-
-    """ streaming """
-    def on_nis_block(self, ws, data):
-        if self.height != data['height']:
-            logging.info("nis/block  %s" % data)
-            self.height = data['height']
-
-    def on_nis_incoming(self, ws, data):
-        logging.info("nis/incoming  %s" % data)
-        pass
-
-    def on_tip_receive(self, ws, data):
-        logging.info("tip/receive  %s" % data)
-        pass
+        logging.info("open: %s" % ws)
 
 
-""" テストコード """
-if __name__ == '__main__':
-    def on_nis_block_overwrite(ws, data):
-        print(data)
-        print("GET!")
+def test():
+    """ テストコード """
+    def _streaming(ws):
+        while True:
+            cmd, data, time_sec = ws.streaming_que.get()
+            print(cmd, data, time_sec)
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('[%(levelname)-6s] [%(threadName)-10s] [%(asctime)-24s] %(message)s')
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.INFO)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("requests").setLevel(logging.WARNING)
 
     ws = WebSocketClient(url="ws://153.122.86.46:8088")
-    # ws.on_nis_block = on_nis_block_overwrite  # 上書きする
     ws.start()
-    print(ws.request(command="bot/info"))
+    threading.Thread(
+        target=_streaming, args=(ws,), daemon=True
+    ).start()
+    ws.login_by_pin_guest(screen=input("screen >> "))
+    ws.login_by_pin_user()
 
     while True:
-        print()
-        screen = input("screen >> ")
-        data = {"screen_name": screen}
-        ok, result = ws.request(command="user/offer", data=data)
-        if not ok and result != "pincode has been already created.":
-            print("# failed login: ", result)
-            continue
+        try:
+            print()
+            command = input("cmd >> ")
+            data = json.loads(input("data >> "))
+            ok, result = ws.request(command=command, data=data)
+            print(ok)
+            print(result)
+        except Exception as e:
+            print("Error", e)
 
-        pin_code = input("pincode >> ")
-        data = {"screen_name": screen, "pincode": int(pin_code)}
-        ok, result = ws.request(command="user/check", data=data)
-        if not ok:
-            print("# failed login: ", result)
-            continue
-
-        print("# login OK!")
-        while True:
-            try:
-                print()
-                command = input("cmd >> ")
-                data = json.loads(input("data >> "))
-                ok, result = ws.request(command=command, data=data)
-                print(ok)
-                print(result)
-            except:
-                pass
+if __name__ == '__main__':
+    test()
